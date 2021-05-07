52340c121ba6a7e42e813abd3f04b300c4d2373b
Nate Chandler-Smith <nate.chandler-smith@accenture.com>
2021-05-07 15:14:33 -0500

    adds source from Spring initializr

---
+++ src/main/java/com/example/javalambdaexperiment/JavaLambdaExperimentApplication.java
@@ -0,0 +1,13 @@
+package com.example.javalambdaexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLambdaExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLambdaExperimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javalambdaexperiment/JavaLambdaExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javalambdaexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLambdaExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
